972f93b42b7fff18f1602f3b6da5d09b2d6575bc
Tarek Mostafa <tarek.mostafa@zalando.de>
2021-06-17 11:46:34 +0200

    another

--- src/motoko_ic_assets/js/vendor/run_repl.js
+++ src/motoko_ic_assets/js/vendor/run_repl.js
@@ -2,7 +2,9 @@ function changeCodeBlock() {
   for (var e = document.getElementsByTagName("pre"), t = 0; t < e.length; t++) {
     var n = e[t],
       i = n.firstChild;
-    if (i && "CODE" === i.tagName) if (i.classList.contains("language-motoko")) n.classList.add("motoko"), appendRun(n, extractConfig(n));
+    if (i && "CODE" === i.tagName)
+      if (i.classList.contains("language-motoko"))
+        n.classList.add("motoko"), appendRun(n, extractConfig(n));
   }
 }
 function extractConfig(e) {
@@ -11,7 +13,8 @@ function extractConfig(e) {
   n && (n += ".mo");
   for (var i = [], o = null, r = 0; r < t.classList.length; r++) {
     var l = t.classList[r];
-    if (l.startsWith("include")) for (var s = l.split("_"), a = 1; a < s.length; a++) i.push(s[a]);
+    if (l.startsWith("include"))
+      for (var s = l.split("_"), a = 1; a < s.length; a++) i.push(s[a]);
     else if (l.startsWith("hook")) {
       o = (s = l.split("_"))[1];
     }
@@ -31,7 +34,11 @@ function highlightCode(e) {
 }
 function saveIncluded(e) {
   for (var t = {}, n = 0; n < e.length; n++) {
-    var i = document.getElementById(e[n]).querySelector("div.content").querySelector("pre").querySelector("code").innerText,
+    var i = document
+        .getElementById(e[n])
+        .querySelector("div.content")
+        .querySelector("pre")
+        .querySelector("code").innerText,
       o = e[n] + ".mo";
     Motoko.saveFile(o, i), (t[o] = i);
   }
@@ -41,10 +48,10 @@ function appendRun(s, a) {
   if ((a.name && Motoko.saveFile(a.name, s.firstChild.innerText), !a.noRepl)) {
     var e,
       t = s.parentNode;
-    console.log("wtf", a, t);
     e = a.lineNumber ? window.withLineNumbers(highlightCode) : highlightCode;
     window.CodeJar(s, e, { tab: "  " });
-    a.lineNumber ? (s.style = "padding-left: calc(35px)") : (s.style = ""), (t.style = "position:relative");
+    a.lineNumber ? (s.style = "padding-left: calc(35px)") : (s.style = ""),
+      (t.style = "position:relative");
     var n = document.createElement("button"),
       d = document.createElement("div");
     (d.classList = "listingblock"),
@@ -62,7 +69,8 @@ function appendRun(s, a) {
           o = a.name || "stdin";
         if ((Motoko.saveFile(o, i), a.hook)) {
           var r = window[a.hook];
-          if ("function" != typeof r) throw new Error(a.hook + " is not a function");
+          if ("function" != typeof r)
+            throw new Error(a.hook + " is not a function");
           e = r.apply(null, [n, i]);
         } else {
           var l = a.include.map(function (e) {
@@ -71,8 +79,14 @@ function appendRun(s, a) {
           e = Motoko.run(l, o);
         }
         (d.innerHTML = ""),
-          e.stderr && (((t = document.createElement("pre")).style = "color:red"), (t.innerText = e.stderr), d.appendChild(t));
-        e.stdout && (((t = document.createElement("pre")).style = "color:green"), (t.innerText = e.stdout), d.appendChild(t));
+          e.stderr &&
+            (((t = document.createElement("pre")).style = "color:red"),
+            (t.innerText = e.stderr),
+            d.appendChild(t));
+        e.stdout &&
+          (((t = document.createElement("pre")).style = "color:green"),
+          (t.innerText = e.stdout),
+          d.appendChild(t));
         highlightCode(s);
       }),
       a.isRun && n.click();
